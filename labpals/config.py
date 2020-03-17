fe0c7db5a21cd19a2b523c184d21760096621704
Armand <armandgonzalezescalante@gmail.com>
2020-03-17 20:20:09 +0100

    sqlite to mysql

--- labpals/config.py
+++ labpals/config.py
@@ -6,6 +6,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'labpals.db')
+    SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
